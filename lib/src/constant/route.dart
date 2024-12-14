7d484bc7fb7d60e4f2a3cf611d64587dee421915
Huy Tran Huu <huyhuytranhuu@gmail.com>
2024-12-14 23:57:01 +0700

    fix route alert

--- lib/src/constant/route.dart
+++ lib/src/constant/route.dart
@@ -2,7 +2,8 @@ enum RoutePath {
 
   home('/', '/'),
   chart('/chart','/chart'),
-  config('/config','/config');
+  config('/config','/config'),
+  alert('/alert','/alert');
 
   const RoutePath(this.relative, this.absolute);
   final String relative, absolute;
