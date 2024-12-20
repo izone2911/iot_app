2e2353d85911a35c289f884d47dd0c8a1eae1fb8
Hoe1802 <thanhhoa18022003@gmai.com>
2024-12-21 04:10:59 +0700

    Thêm tin tức

---
+++ lib/src/network/news_service.dart
@@ -0,0 +1,32 @@
+// network/news_service.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../constant/news_model.dart';
+
+class NewsApi {
+  // for news home screen
+  List<NewsModel> dataStore = [];
+  Future<void> getNews() async {
+    Uri url = Uri.parse(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=5021309941d54963b4be06f064ab6bf5");
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+    if (jsonData["status"] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null &&
+            element['description'] != null &&
+            element['author'] != null &&
+            element['content'] != null) {
+          NewsModel newsModel = NewsModel(
+            title: element['title'], // name must be same fron api
+            urlToImage: element['urlToImage'],
+            description: element['description'],
+            author: element['author'],
+            content: element['content'],
+          );
+          dataStore.add(newsModel);
+        }
+      });
+    }
+  }
+}
